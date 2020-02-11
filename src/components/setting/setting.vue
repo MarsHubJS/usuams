66ed61f8737343c5c3d96e78a871f53e259a49c2
Encaik <843391622@qq.com>
2020-02-11 16:44:21 +0800

    添加动态主题修改

--- src/components/setting/setting.vue
+++ src/components/setting/setting.vue
@@ -13,9 +13,30 @@
       @close="onClose"
       :visible="visible"
     >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <div class="hello">
+        <h1>{{ msg }}</h1>
+        <a-button type="primary" @click="changeTheme('#992777')">默认</a-button
+        ><br /><br />
+        <a-button type="primary" @click="changeTheme('#F5222D')">薄暮</a-button
+        ><br /><br />
+        <a-button type="primary" @click="changeTheme('#FA541C')">火山</a-button
+        ><br /><br />
+        <a-button type="primary" @click="changeTheme('#FAAD14')">日暮</a-button
+        ><br /><br />
+        <a-button type="primary" @click="changeTheme('#13C2C2')">明青</a-button
+        ><br /><br />
+        <a-button type="primary" @click="changeTheme('#52C41A')"
+          >极光绿</a-button
+        ><br /><br />
+        <a-button type="primary" @click="changeTheme('#1890FF')"
+          >拂晓蓝</a-button
+        ><br /><br />
+        <a-button type="primary" @click="changeTheme('#2F54EB')"
+          >极客蓝</a-button
+        ><br /><br />
+        <a-button type="primary" @click="changeTheme('#722ED1')">酱紫</a-button
+        ><br /><br />
+      </div>
     </a-drawer>
   </div>
 </template>
@@ -33,6 +54,11 @@ export default {
     },
     onClose() {
       this.visible = false;
+    },
+    changeTheme(primaryColor) {
+      window.less.modifyVars({
+        "@primary-color": primaryColor
+      });
     }
   }
 };
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
 // vue.config.js
 // const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 // const CompressionPlugin = require("compression-webpack-plugin");
+const AntDesignThemePlugin = require("antd-theme-webpack-plugin");
+const path = require("path");
+
+const options = {
+  antDir: path.join(__dirname, "./node_modules/ant-design-vue"), //antd包位置
+  stylesDir: path.join(__dirname, "./src/style/theme"), //主题文件所在文件夹
+  varFile: path.join(__dirname, "./src/style/theme/red.less"), // 自定义默认的主题色
+  mainLessFile: path.join(__dirname, "./src/style/theme/index.less"), // 项目中其他自定义的样式（如果不需要动态修改其他样式，该文件可以为空）
+  outputFilePath: path.join(__dirname, "./public/color.less"), //提取的less文件输出到什么地方
+  themeVariables: ["@primary-color"], //要改变的主题变量
+  indexFileName: "./public/index.html", // index.html所在位置
+  generateOnce: false // 是否只生成一次（if you don't want to generate color.less on each chnage in code to make build process fast in development mode, assign it true value. But if you have new changes in your styles, you need to re-run your build process npm start.）
+};
 
 module.exports = {
   publicPath: "./",
@@ -8,23 +21,26 @@ module.exports = {
     loaderOptions: {
       less: {
         modifyVars: {
-          "primary-color": "#6584F2", // 全局主色
-          "link-color": "#1890ff", // 链接色
-          "success-color": "#52c41a", // 成功色
-          "warning-color": "#faad14", // 警告色
-          "error-color": "#f5222d", // 错误色
-          "font-size-base": "14px", // 主字号
-          "heading-color": "rgba(0, 0, 0, .85)", // 标题色
-          "text-color": "rgba(0, 0, 0, .65)", // 主文本色
-          "text-color-secondary": "rgba(0, 0, 0, .45)", // 次文本色
-          "disabled-color": "rgba(0, 0, 0, .25)", // 失效色
-          "border-radius-base": "5px", // 组件/浮层圆角
-          "border-color-base": "#d9d9d9", // 边框色
-          "box-shadow-base": "0 2px 8px rgba(0, 0, 0, .15)" // 浮层阴影
+          // "primary-color": "#6584F2", // 全局主色
+          // "link-color": "#1890ff", // 链接色
+          // "success-color": "#52c41a", // 成功色
+          // "warning-color": "#faad14", // 警告色
+          // "error-color": "#f5222d", // 错误色
+          // "font-size-base": "14px", // 主字号
+          // "heading-color": "rgba(0, 0, 0, .85)", // 标题色
+          // "text-color": "rgba(0, 0, 0, .65)", // 主文本色
+          // "text-color-secondary": "rgba(0, 0, 0, .45)", // 次文本色
+          // "disabled-color": "rgba(0, 0, 0, .25)", // 失效色
+          // "border-radius-base": "5px", // 组件/浮层圆角
+          // "border-color-base": "#d9d9d9", // 边框色
+          // "box-shadow-base": "0 2px 8px rgba(0, 0, 0, .15)" // 浮层阴影
         },
         javascriptEnabled: true
       }
     }
+  },
+  configureWebpack: {
+    plugins: [new AntDesignThemePlugin(options)]
   }
   // lintOnSave: true,
   // productionSourceMap: false,
